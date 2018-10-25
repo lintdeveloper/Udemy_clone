de111f0fae4ffca73d2db3e611c7d8c8f8b195b8
Musa Musa <musamusa739@gmail.com>
2018-10-25 10:59:15 +0100

    Added Data models

---
+++ models/course.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    desc: String,
+    wistiaId: String,
+    price: Number,
+    ownByTeacher: {type: Schema.Types.ObjectId, ref: 'User'},
+    ownByStudent: [{
+        user: {type: Schema.Types.ObjectId, ref: 'User'},
+    }],
+    totalStudents: Number
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema
+const userSchema = new Schema({
+    
+    email: {type: String, unique: true, lowercase: true},
+    profile: {
+        name: {type: String, default: ''},
+        picture: {type: String, default: ''}
+    },
+
+    coursesTeach: [{
+        course: {type: Schema.Types.ObjectId, ref: "Course"}
+    }],
+
+    coursesTaken: [{
+        course: {type: Schema.Types.ObjectId, ref: "Course"}
+    }]
+});
+
+module.exports = mongoose.model('User', userSchema);
